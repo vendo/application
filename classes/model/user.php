6ebac36eaa97eb445d85a8d944b1009bee5d18f1
Jeremy Bush <contractfrombelow@gmail.com>
2010-11-03 06:40:02 -0500

    making user model more extendable in general applications

--- classes/model/user.php
+++ classes/model/user.php
@@ -7,7 +7,84 @@
  * @copyright  (c) 2010 Jeremy Bush
  * @license    http://github.com/zombor/Vendo/raw/master/LICENSE
  */
-class Model_User extends Model_Vendo_User
+class Model_User extends AutoModeler_ORM
 {
-	
+	protected $_table_name = 'users';
+
+	protected $_data = array(
+		'id' => '',
+		'email' => '',
+		'first_name' => '',
+		'last_name' => '',
+		'password' => '',
+		'address_id' => NULL,
+	);
+
+	protected $_rules = array(
+		'email' => array('not_empty', 'email'),
+		'first_name' => array('not_empty'),
+		'last_name' => array('not_empty'),
+		'password' => array('not_empty'),
+		'address_id' => array('numeric'),
+	);
+
+	protected $_callbacks = array(
+		'email' => 'check_unique_email',
+	);
+
+	protected $_has_many = array(
+		'vendo_roles',
+	);
+
+	protected $_shopping_cart;
+
+	/**
+	 * Sets a value to this object. Used for hashing passwords for the user
+	 * 
+	 * @param string $key   the key to set
+	 * @param mixed  $value the value to set
+	 * 
+	 * @return null
+	 */
+	public function __set($key, $value)
+	{
+		if ('password' == $key AND $value)
+		{
+			$value = Auth::instance()->hash_password($value);
+		}
+
+		parent::__set($key, $value);
+	}
+
+	/**
+	 * Constructor to load the object by an email address
+	 * 
+	 * @param mixed $id the id to load by. A numerical ID or an email address
+	 * 
+	 * @return null
+	 */
+	public function __construct($id = NULL)
+	{
+		if ( ! is_numeric($id) AND NULL != $id)
+		{
+			// try and get a row with this ID
+			$data = db::select_array(array_keys($this->_data))
+				->from($this->_table_name)
+				->where('email', '=', $id)
+				->execute($this->_db);
+
+			// try and assign the data
+			if (count($data) == 1 AND $data = $data->current())
+			{
+				foreach ($data as $key => $value)
+					$this->_data[$key] = $value;
+			}
+		}
+		else
+		{
+			parent::__construct($id);
+		}
+
+		$this->_shopping_cart = new Model_Order;
+	}
 }
